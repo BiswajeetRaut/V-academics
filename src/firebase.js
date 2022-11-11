dc5a7fc1132f84490c7b7955455b00af1a372c7d
BiswajeetRaut <85820671+BiswajeetRaut@users.noreply.github.com>
2022-11-12 02:59:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDJYPYN-pCQ5SFR313ECWi9Xcvqa2rVNCc",
-  authDomain: "v-academics.firebaseapp.com",
-  projectId: "v-academics",
-  storageBucket: "v-academics.appspot.com",
-  messagingSenderId: "207777206779",
-  appId: "1:207777206779:web:05fdb1eb5dfa70d9357b5e",
-  measurementId: "G-5NH0ZDX2DK"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
   };
 // const firebaseApp = firebase.initializeApp(fireba)
 const firebaseApp = firebase.initializeApp(firebaseConfig);
